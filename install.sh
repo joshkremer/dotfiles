173af5779cdde9c674c6f25736ab0ecebc6a56f6
joshkremer <joshkremer@gmail.com>
2019-06-07 05:55:02 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-mkdir ~/workspace
-#install ohmyzsh
-./zsh.sh
 #install homebrew
 CI=1 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 #install utilities
 ./brew.sh
 #install macos apps
 ./apps.sh
+./zsh.sh
+./macos.#!/bin/sh
