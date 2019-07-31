c29d8aa0282f2aa6bb92efe50440491eb693b2da
joshkremer <joshkremer@gmail.com>
2019-07-30 17:30:40 -0700

    Update 4_zsh.sh

--- 4_zsh.sh
+++ 4_zsh.sh
@@ -4,6 +4,5 @@ defaults write com.apple.finder AppleShowAllFiles YES
 
 cp .zshrc ~/.zshrc
 cp .vimrc ~/.vimrc
-cp TheOne.zsh-theme ~/.oh-my-zsh/
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp TheOne.zsh-theme ~/.oh-my-zsh/
