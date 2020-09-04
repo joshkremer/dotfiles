4c47fe92c324092e51dda2e90a0e1f0a24767ae9
joshkremer <joshkremer@gmail.com>
2020-09-04 05:44:35 -0700

    updates

--- 5_pip_list.sh
+++ 5_pip_list.sh
@@ -9,3 +9,10 @@ pip install tqdm
 pip install pyfiglet
 pip install psutil
 pip install wget
+pip install django-crispy-forms
+pip install Pillow
+pip install socketlabs-injectionapi
+pip install socketlabs
+pip install google-api-python-client==1.7.9
+pip install google-auth-httplib2==0.0.3
+pip install google-auth-oauthlib==0.4.0
