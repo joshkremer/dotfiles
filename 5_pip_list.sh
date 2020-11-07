69ff281da8d21b503aa28cf151ed4938f1d4aaac
joshkremer <joshkremer@gmail.com>
2020-11-07 07:13:55 -0800

    added pretty_errors

--- 5_pip_list.sh
+++ 5_pip_list.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
-pip install pretty-errors
-pip install shopifyapi
-pip install requests
-pip install django-rq
 pip install arrow
-pip install tqdm
-pip install pyfiglet
-pip install psutil
-pip install wget
+pip install bigcommerce
+pip install bs4
 pip install django-crispy-forms
-pip install Pillow
-pip install socketlabs-injectionapi
-pip install socketlabs
+pip install django-rq
 pip install google-api-python-client==1.7.9
 pip install google-auth-httplib2==0.0.3
 pip install google-auth-oauthlib==0.4.0
+pip install mailchimp3
+pip install netifaces
+pip install Pillow
+pip install pretty-errors
+pip install psutil
+pip install pydrive
+pip install pyfiglet
+pip install requests
+pip install shopifyapi
+pip install socketlabs-injectionapi
+pip install socketlabs
+pip install tqdm
+pip install wget
+
+# run manually
+python - m pretty_errors
