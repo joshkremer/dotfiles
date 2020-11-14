f434d18f10ba17aefe51f16ec7d9c414d2458871
joshkremer <joshkremer@gmail.com>
2020-11-14 09:09:18 -0800

    Update 2_apps.sh

--- 2_apps.sh
+++ 2_apps.sh
@@ -1,38 +1,39 @@
 #!/usr/bin/env bash
 
-brew cask install airfoil
-brew cask install google-chrome
-brew cask install google-chat
-brew cask install firefox
-brew cask install spectacle
-brew cask install alfred
-brew cask install cloudapp
-brew cask install github
-brew cask install iterm2
-brew cask install keka
-brew cask install jumpcut
-brew cask install local
-brew cask install transmit
-brew cask install visual-studio-code
-brew cask install github
-brew cask install timely
-brew cask install balenaetcher
-brew cask install signal
+brew tap homebrew/cask-drivers
+
 brew cask install 1password
-brew cask install sequel-pro
+brew cask install airfoil
+brew cask install alfred
+brew cask install balenaetcher
+brew cask install cloudapp
+brew cask install discord
+brew cask install firefox
+brew cask install github
+brew cask install google-drive-file-stream
+brew cask install google-chat
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install jumpcut
+brew install jq
+brew cask install keka
+brew cask install local
+brew install ncdu
+brew cask install quickbooks-online
 brew cask install screaming-frog-seo-spider
+brew cask install sequel-pro
+brew tap shopify/shopify
+brew cask install signal
+brew cask install skype
+brew cask install slack
+brew cask install spectacle
 brew cask install spotify
+brew cask install steam
+brew install themekit
+brew cask install timely
+brew cask install transmit
 brew cask install tunnelbear
+brew cask install visual-studio-code
 brew cask install whatsapp
 brew cask install zoomus
-brew cask install slack
-brew cask install skype
-brew cask install steam
-brew cask install quickbooks-online
-brew cask install jq
-brew cask install ncdu
-brew tap shopify/shopify
-brew install themekit
-brew cask install google-drive-file-stream
-brew tap homebrew/cask-drivers
 brew cask install zsa-wally
