b4fe449a392321e36404240474af3a66f8fab828
joshkremer <joshkremer@gmail.com>
2020-11-22 13:13:07 -0800

    updates

--- 0_backup_config_files.sh
+++ 0_backup_config_files.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-find ~/Documents/workspace -type f -name "*code-workspace" > vscode-workspace-files.txt
 
 cp ~/.zshrc .
 cp -r ~/.ssh .
+
+#Spectacle 
 cp /Users/joshkremer/Library/Application\ Support/Spectacle/Shortcuts.json .
 
+#VSCode workspace files
+find ~/Documents/workspace -type f -name "*code-workspace" > vscode-workspace-files.txt
 mkdir vscode-workspace-files
 
 cat vscode-workspace-files.txt | while read line
@@ -17,4 +20,8 @@ done
 zip -r vscode-workspace-files.zip vscode-workspace-files
 rm -rf vscode-workspace-files
 rm vscode-workspace-files.txt
+
+#Automator workflows
+mkdir automator-workflows
+cp -a ~/Library/Services/. automator-workflows/
 
---
+++ 7_setup_automator_workflows.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# copy automator workflow files to allow for custom keyboard shorcuts
+cp -a automator-workflows/. ~/Library/Services/
