2097a94d1f8a142c865ea1213a28f93efc233ff1
joshkremer <joshkremer@gmail.com>
2020-11-22 09:14:59 -0800

    updates

--- 0_backup_config_files.sh
+++ 0_backup_config_files.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
-find ~/Documents/workspace -type f -name "*code-workspace" > vscode_workspace_files.txt
+find ~/Documents/workspace -type f -name "*code-workspace" > vscode-workspace-files.txt
 
 cp ~/.zshrc .
-cp -r ~/.ssh/ .
+cp -r ~/.ssh .
+cp /Users/joshkremer/Library/Application\ Support/Spectacle/Shortcuts.json .
 
-cat vscode_workspace_files.txt | while read line
+mkdir vscode-workspace-files
+
+cat vscode-workspace-files.txt | while read line
 do
     echo "backing up: $line"
-    cp $line .
+    cp $line vscode-workspace-files
 done
 
+zip -r vscode-workspace-files.zip vscode-workspace-files
+rm -rf vscode-workspace-files
+rm vscode-workspace-files.txt
 
