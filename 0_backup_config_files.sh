5ba7605c640073052405a308de7e0ec887ced38e
joshkremer <joshkremer@gmail.com>
2021-03-01 14:18:46 -0800

    updates for pycharm

--- 0_backup_config_files.sh
+++ 0_backup_config_files.sh
@@ -1,25 +1,10 @@
 #!/usr/bin/env bash
 
-
 cp ~/.zshrc .
 cp -r ~/.ssh .
 
 #Spectacle 
 cp /Users/joshkremer/Library/Application\ Support/Spectacle/Shortcuts.json .
-
-#VSCode workspace files
-find ~/Documents/workspace -type f -name "*code-workspace" > vscode-workspace-files.txt
-mkdir vscode-workspace-files
-
-cat vscode-workspace-files.txt | while read line
-do
-    echo "backing up: $line"
-    cp $line vscode-workspace-files
-done
-
-zip -r vscode-workspace-files.zip vscode-workspace-files
-rm -rf vscode-workspace-files
-rm vscode-workspace-files.txt
 
 #Automator workflows
 mkdir automator-workflows
--- 2_apps.sh
+++ 2_apps.sh
@@ -3,7 +3,6 @@
 brew tap homebrew/cask-drivers
 
 brew cask install 1password
-brew cask install airfoil
 brew cask install alfred
 brew cask install balenaetcher
 brew cask install cloudapp
@@ -11,7 +10,6 @@ brew cask install discord
 brew cask install firefox
 brew cask install github
 brew cask install google-drive-file-stream
-brew cask install google-chat
 brew cask install google-chrome
 brew cask install iterm2
 brew cask install jumpcut
@@ -24,11 +22,11 @@ brew cask install screaming-frog-seo-spider
 brew cask install sequel-pro
 brew tap shopify/shopify
 brew cask install signal
-brew cask install skype
 brew cask install slack
 brew cask install spectacle
 brew cask install spotify
 brew cask install steam
+brew cask install telegram
 brew install themekit
 brew cask install timely
 brew cask install transmit
