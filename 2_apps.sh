f22144e5f69acf549d6262171cb60bbba1342f2c
joshkremer <joshkremer@gmail.com>
2021-03-01 14:42:32 -0800

    Update 2_apps.sh

--- 2_apps.sh
+++ 2_apps.sh
@@ -2,37 +2,38 @@
 
 brew tap homebrew/cask-drivers
 
-brew cask install 1password
-brew cask install alfred
-brew cask install balenaetcher
-brew cask install cloudapp
-brew cask install discord
-brew cask install firefox
-brew cask install github
-brew cask install google-drive-file-stream
-brew cask install google-chrome
-brew cask install iterm2
-brew cask install jumpcut
+brew install --cask 1password
+brew install -cask alfred
+brew install -cask balenaetcher
+brew install -cask cloudapp
+brew install -cask discord
+brew install -cask firefox
+brew install -cask github
+brew install -cask google-drive-file-stream
+brew install -cask google-chrome
+brew install -cask iterm2
+brew install -cask jumpcut
 brew install jq
-brew cask install keka
-brew cask install local
+brew install -cask keka
+brew install -cask local
 brew install ncdu
-brew cask install quickbooks-online
-brew cask install screaming-frog-seo-spider
-brew cask install sequel-pro
+brew install --cask pycharm
+brew install -cask quickbooks-online
+brew install -cask screaming-frog-seo-spider
+brew install -cask sequel-pro
 brew tap shopify/shopify
-brew cask install signal
-brew cask install slack
-brew cask install spectacle
-brew cask install spotify
-brew cask install steam
-brew cask install telegram
+brew install -cask signal
+brew install -cask slack
+brew install -cask spectacle
+brew install -cask spotify
+brew install -cask steam
+brew install -cask telegram
 brew install themekit
-brew cask install timely
-brew cask install transmit
-brew cask install tunnelbear
-brew cask install visual-studio-code
-brew cask install virtualbox
-brew cask install whatsapp
-brew cask install zoomus
-brew cask install zsa-wally
+brew install -cask timely
+brew install -cask transmit
+brew install -cask tunnelbear
+brew install -cask visual-studio-code
+brew install -cask virtualbox
+brew install -cask whatsapp
+brew install -cask zoomus
+brew install -cask zsa-wally
